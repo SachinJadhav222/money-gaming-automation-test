4f5f5e9ad90f52a221a7cffb19291ad88581b0b9
sachin.jadhav222 <sachin.jadhav222@yahoo.com>
2021-04-01 22:48:38 +0100

    allure report setting

--- cypress/locators/loc01.homePage.js
+++ cypress/locators/loc01.homePage.js
@@ -1,5 +1,5 @@
 module.exports = {
-	loc01_join_now_button: '//a[contains(text(),"Join Now!")]',
+	//loc01_join_now_button: '//a[contains(text(),"Join Now!")]',
 	
-
+	loc01_join_now_button:'.newUser'
 }
